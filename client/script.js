40e47ef264a9bc4cdd76a0348471c83f597b40ec
Gaurav Dhamane <75262183+gaurav-dhamane@users.noreply.github.com>
2024-05-09 03:04:51 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -72,15 +72,47 @@ const handleSubmit= async (e) => {
 
     // fetch data from server
 
-    const response = await fetch('https://friday-am6k.onrender.com',{
-        method: 'POST',
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
+
+    const fetch = require('node-fetch');
+
+const url = 'https://chat-gpt26.p.rapidapi.com/';
+const options = {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json',
+    'Content-Type': 'application/json',
+    'X-RapidAPI-Key': 'b1af9eb107msh66828772b903b03p18112cjsn28cd1c095891',
+    'X-RapidAPI-Host': 'chat-gpt26.p.rapidapi.com'
+  },
+  body: {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: 'Hello'
+      }
+    ]
+  }
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+    //
+
+    // const response = await fetch('https://friday-am6k.onrender.com',{
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type':'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         prompt: data.get('prompt')
+    //     })
+    // })
     clearInterval(loadInterval);
     messageDiv.innerHTML= " ";
     if(response.ok){
